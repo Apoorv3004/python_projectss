8e03902b3d69a97f47731b47b76e0adc51b01b05
Apoorv3004 <apoorv861@gmail.com>
2026-06-22 13:58:31 +0530

    Add files via upload

---
+++ BMI calculator.py
@@ -0,0 +1,6 @@
+print("lets calculate your body mass index")
+height =float(input("enter your height in metre"))
+weight = float(input("enter your weight in kg"))
+bmi = weight / (height ** 2)
+print("your bmi is :" + str(bmi))
+
