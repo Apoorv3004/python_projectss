9e9d096b85b885cf90a8cceb79a37605e58212bf
Apoorv3004 <apoorv861@gmail.com>
2026-06-30 13:24:22 +0530

    Improve input validation for user choice
    Add input validation for user choice in game.

--- rock paper scissors.py
+++ rock paper scissors.py
@@ -3,11 +3,11 @@ user_choice = int(input("What do you choose? type 0 for Rock, 1 for scissors, 2 
 computer_choice = random.randint(0,2)
 print(f"Computer chose {computer_choice}")
 
-if user_choice == 0 and computer_choice == 2:
+if user_choice >=3 or user_choice <0:
+    print("You typed a invalid number, You lose!")
+elif user_choice == 0 and computer_choice == 2:
     print("You win!")
 elif computer_choice == user_choice:
     print("It\'s a draw!")
 elif computer_choice > user_choice:
     print("You lose!")
-else:
-    print("You typed a invalid number")
